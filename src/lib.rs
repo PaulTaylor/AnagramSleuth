3e4914ec7a099d9225bfcecd959d10024102bd7b
Paul Taylor <184124+PaulTaylor@users.noreply.github.com>
2023-06-17 15:22:59 +0100

    first pass at letters done

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,57 @@
-use wasm_bindgen::prelude::*;
+use regex::Regex;
+use std::collections::HashMap;
+use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
-pub fn letters(input: String, wordlist: Vec<String>) -> usize {
-    let mut i = 0;
-    for line in wordlist {
-        let trimmed = line.trim().to_lowercase();
+pub fn letters(input: &str, wordlist: &str, min_word_length: usize) -> String {
+    let downcased = input.to_ascii_lowercase();
+    let cleaned = downcased.trim();
+
+    let mut in_counts = HashMap::new();
+    for c in cleaned.chars() {
+        in_counts.entry(c).and_modify(|v| *v += 1).or_insert(1usize);
     }
-    i
+
+    let pattern = format!("^[{cleaned}]{{{min_word_length},}}$");
+    let matcher = Regex::new(&pattern).expect("The search pattern did not compile :(");
+
+    let mut candidates = Vec::new();
+    for line in wordlist.lines() {
+        // Assumption: line is trimmed and lowercased
+        if matcher.is_match(line) {
+            let mut cand_counts = HashMap::new();
+            for c in line.chars() {
+                cand_counts
+                    .entry(c)
+                    .and_modify(|v| *v += 1)
+                    .or_insert(1usize);
+            }
+
+            if cand_counts.into_iter().all(|(ch, f)| f <= in_counts[&ch]) {
+                candidates.push(line.to_string());
+            }
+        }
+    }
+
+    candidates.sort_unstable_by(|a, b| a.len().cmp(&b.len()).reverse());
+
+    // Because we can't currently return a Vec<String> - settle for a comma seperated String
+    candidates.join(",")
 }
 
 #[test]
 fn test_letters() {
     use flate2::read::GzDecoder;
     use std::fs::File;
+    use std::io::Read;
 
     let mut reader = GzDecoder::new(File::open("wordlist.gz").unwrap());
-    let buf = &mut String::new();
-    let _ = reader.read_to_string(buf);
+    let wordlist = &mut String::new();
+    reader
+        .read_to_string(wordlist)
+        .expect("The wordlist was not read :(");
 
-    let wordlist: &[String] = buf.lines().map(|l| l.trim().to_lowercase()).collect();
-
-    assert_eq!(letters("hello".into(), wordlist), 1)
+    assert_eq!(letters("hello", wordlist, 3), "hello,hell,hole,ell,hoe");
+    assert_eq!(letters("bye", wordlist, 3), "bye");
+    assert_eq!(letters("bye", wordlist, 4), "");
 }
