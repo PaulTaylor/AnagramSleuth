242cfc2fd2058b30398e7aeb3f96907ac3d2f883
Paul Taylor <184124+PaulTaylor@users.noreply.github.com>
2023-06-17 21:04:13 +0100

    update build script

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,4 +1,5 @@
+mkdir -p ./public/pkg
 wasm-pack build --release --target web
-cp -v ./pkg/ana* ./public/
+cp -v ./pkg/ana* ./public/pkg/
 cp -v index.html wordlist.gz ./public/
 wrangler pages deploy public
